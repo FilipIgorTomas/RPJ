817bb01e6bc0fe60b8932ebf379a403c8845172e
Filip Igor Tomáš <147716326+FilipIgorTomas@users.noreply.github.com>
2024-10-27 21:59:50 +0100

    zmena DB

--- db/db_skiservis.sql
+++ db/db_skiservis.sql
@@ -9,10 +9,67 @@ GO
 
 DROP SCHEMA ski
 
-CREATE TABLE komp.customer_login
-( id_customer		INT				   	IDENTITY(1,1)
-, [mail]     		VARCHAR(40)	   		NOT NULL
-, [password]		CHAR(12)			NOT NULL
-, CONSTRAINT customer_login_pk PRIMARY KEY(id_customer)
+CREATE TABLE ski.customer
+( id_customer		INT				IDENTITY(1,1)
+, mail       		VARCHAR(40)     NOT NULL
+, [password]		VARCHAR(30)     NOT NULL
+, first_name        VARCHAR(255)    NOT NULL
+, last_name         VARCHAR(255)    NOT NULL
+, phone_number      CHAR(12)        NOT NULL
+, CONSTRAINT customer_pk PRIMARY KEY(id_customer)
 )
 GO
+
+CREATE TABLE ski.services
+( id_service	    INT             IDENTITY(1,1)
+, services_name     VARCHAR(30)     NOT NULL
+, price             MONEY           NOT NULL
+, CONSTRAINT services_pk PRIMARY KEY(id_service)
+)
+GO
+
+CREATE TABLE ski.rentals
+( id_rental         INT             IDENTITY(1,1)
+, product           VARCHAR(30)     NOT NULL
+, price             MONEY           NOT NULL
+, content           VARCHAR(30)     NOT NULL
+, CONSTRAINT rentals_pk PRIMARY KEY(id_rental)
+)
+GO
+
+CREATE TABLE ski.categories
+( id_category       INT         IDENTITY(1,1)
+, category_name     VARCHAR(30) NOT NULL
+, CONSTRAINT categories_pk PRIMARY KEY(id_category)
+)
+GO
+
+CREATE TABLE ski.buy
+( id_buy            INT         IDENTITY(1,1)
+, id_category       INT         NOT NULL
+, product_name      VARCHAR(30) NOT NULL
+, price             MONEY       NOT NULL
+, CONSTRAINT buy_pk PRIMARY KEY(id_buy)
+, CONSTRAINT category_f1 FOREIGN KEY(id_category) REFERENCES ski.categories(id_category)
+)
+GO
+
+CREATE TABLE ski.reservations
+( id_reservation     INT            IDENTITY(1,1)
+, id_customer        INT            NOT NULL
+, id_service         INT            NOT NULL
+, CONSTRAINT reservation_pk PRIMARY KEY(id_reservation)
+, CONSTRAINT customer_f1 FOREIGN KEY(id_customer) REFERENCES ski.customer(id_customer)
+, CONSTRAINT service_f1 FOREIGN KEY(id_service) REFERENCES ski.services(id_service)
+)
+GO
+
+DROP TABLE ski.reservations
+DROP TABLE ski.services
+DROP TABLE ski.rentals
+DROP TABLE ski.buy
+DROP TABLE ski.categories
+
+DROP DATABASE db_skiservis
+
+INSERT INTO ski.customer (mail, [password], first_name, last_name, phone_number) VALUES ('customer@gmail.com', 'Hajdfns', 'John', 'Doe', '421905574321')
